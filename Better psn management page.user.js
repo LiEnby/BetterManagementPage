68c59cd8a059ac04159f4a76a36641f9c1450a9a
Bluzume <39113159+KuromeSan@users.noreply.github.com>
2020-08-09 03:18:15 +1200

    Update Better psn management page.user.js

--- Better psn management page.user.js
+++ Better psn management page.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Better psn management page
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Allows for defualt avatar and setitng realname to anything!!
-// @author       You
+// @author       SilicaAndPina
 // @match        https://id.sonyentertainmentnetwork.com/*
 // @grant        none
 // ==/UserScript==
